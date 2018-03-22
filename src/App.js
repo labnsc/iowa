963c643ce281f97235251af75aaaa5141674b29b
Mario Perez <mapreal19@gmail.com>
2018-03-22 13:15:04 +0100

    Upload & Download zip

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
+import JSZip from 'jszip';
+import FileSaver from 'file-saver';
+
 import logo from './logo.svg';
+
 import './App.css';
 
+// TODO: Do parser
+const parse = contents => {
+  return contents;
+};
+
+const downloadZip = zip =>
+  zip.generateAsync({ type: "blob" })
+    .then(content => FileSaver.saveAs(content, "example.zip"));
+
 class App extends Component {
+  handleFiles = event => {
+    const zip = new JSZip();
+
+    Array.from(event.target.files).forEach((file, index, files) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        const parsedContents = parse(reader.result);
+        zip.file(file.name, parsedContents);
+
+        if (index === files.length - 1) downloadZip(zip);
+      };
+      reader.readAsText(file);
+    });
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,7 +38,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
         <p className="App-intro">
+          <input type="file" id="input" multiple onChange={this.handleFiles} />
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
