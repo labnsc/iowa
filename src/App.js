151fc98f9fc004e03924e0986501ed3a58f36dca
Mario Perez <mapreal19@gmail.com>
2018-03-27 16:49:42 +0200

    IOWA in 3

--- src/App.js
+++ src/App.js
@@ -6,26 +6,64 @@ import logo from './logo.svg';
 
 import './App.css';
 
-// TODO: Do parser
-const parse = contents => {
-  return contents;
+const stimulusCode = line => line.match(/S\s*(\d+)/)[1];
+
+export const parse = contents => {
+  let trialCount = 0;
+  const lines = contents.split("\n");
+  let output = '';
+
+  lines.forEach(line => {
+    if (!line.match(/^Mk\d+=S/i)) {
+      output += `${line}\n`;
+      return;
+    }
+    const code = stimulusCode(line);
+
+    if (code === '333' || code === '111') {
+      trialCount++;
+      let newCode;
+
+      switch(true) {
+        case trialCount <= 50:
+          newCode = code === '333' ? '22' : '11';
+          break;
+        case trialCount <= 100:
+          newCode = code === '333' ? '44' : '33';
+          break;
+        default:
+          newCode = code === '333' ? '66' : '55';
+      }
+
+      const parsedLine = line.replace(/S\s*\d+/, `S${newCode}`);
+      output += `${parsedLine}\n`;
+    }
+    else {
+      output += `${line}\n`;
+    }
+  });
+
+  return output;
 };
 
 const downloadZip = zip =>
   zip.generateAsync({ type: "blob" })
-    .then(content => FileSaver.saveAs(content, "example.zip"));
+    .then(content => FileSaver.saveAs(content, "iowa-in-three.zip"));
 
 class App extends Component {
   handleFiles = event => {
     const zip = new JSZip();
+    const filesArray = Array.from(event.target.files);
+    let processed = 0;
 
-    Array.from(event.target.files).forEach((file, index, files) => {
+    filesArray.forEach(file => {
       const reader = new FileReader();
       reader.onload = () => {
         const parsedContents = parse(reader.result);
         zip.file(file.name, parsedContents);
+        processed++;
 
-        if (index === files.length - 1) downloadZip(zip);
+        if (filesArray.length === processed) downloadZip(zip);
       };
       reader.readAsText(file);
     });
@@ -36,13 +74,20 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Labnsc scripts</h1>
         </header>
 
+        <h1>IOWA</h1>
+        <h3>IOWA in 3</h3>
         <p className="App-intro">
-          <input type="file" id="input" multiple onChange={this.handleFiles} />
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Choose the <code>.vmrk</code> files you wish to convert
         </p>
+
+        <p>
+          <input type="file" id="input" multiple onChange={this.handleFiles} />
+        </p>
+
+        <hr/>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { parse } from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('parse()', () => {
+  it('changes S333 for S22', () => {
+    const contents = [
+      "Brain Vision",
+      "Mk2=Stimulus,S  5,13723,1,0",
+      "Mk3=Stimulus,S  1,13724,1,0",
+      "Mk4=Stimulus,S 11,14565,1,0",
+      "Mk5=Stimulus,S333,15323,1,0",
+    ].join("\n");
+
+    const actual = parse(contents);
+    const expected = [
+      "Brain Vision",
+      "Mk2=Stimulus,S  5,13723,1,0",
+      "Mk3=Stimulus,S  1,13724,1,0",
+      "Mk4=Stimulus,S 11,14565,1,0",
+      "Mk5=Stimulus,S22,15323,1,0",
+      "",
+    ].join("\n");
+
+    expect(actual).toEqual(expected);
+  });
 });
