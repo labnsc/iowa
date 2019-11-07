ce1ad1e07ee80d03dd6ea0b73716065889b7ffde
Mario Perez <mapreal19@gmail.com>
2019-11-07 17:00:47 +0100

    Updates

---
+++ precache-manifest.17eaa8066bb740afbdd31765439d2489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9262ebf979b64cd88c8a",
+    "url": "/iowa/static/css/main.4b93e502.chunk.css"
+  },
+  {
+    "revision": "9262ebf979b64cd88c8a",
+    "url": "/iowa/static/js/main.3d2ed055.chunk.js"
+  },
+  {
+    "revision": "8f47e0d6a6ebc0de6e54",
+    "url": "/iowa/static/js/runtime~main.58eaae6d.js"
+  },
+  {
+    "revision": "2b2fef95e91a4b8e4d77",
+    "url": "/iowa/static/js/2.de1e4b92.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/iowa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "28fcb8f86b6d100970ff4c3c7c98673f",
+    "url": "/iowa/index.html"
+  }
+];
